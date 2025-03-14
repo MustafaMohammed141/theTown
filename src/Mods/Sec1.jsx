a133b5a375f8214984c0ffdde6f1a4c80f2d42f0
MustafaMohammed141 <mostafamohamed12434@gmail.com>
2025-03-14 09:55:09 +0200

    something

---
+++ src/Mods/Sec1.jsx
@@ -0,0 +1,25 @@
+import "../Style/Sec1.css";
+
+function Sec1() {
+	return (
+		<section>
+			<div>
+				<img src="public/introSection.jpg" />
+			</div>
+			<div>
+				<h1>Introduction</h1>
+				<p>
+					The Town is <b>simple CSS template</b> built on HTML CSS and this is a little parallax layout that you can use for your website
+				</p>
+				<br />
+				<p>
+					please tell your friends about our site <a href="#">YourSite</a>. Thank you. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+					Itaque, nisi tenetur.
+				</p>
+				<button>Read More</button>
+			</div>
+		</section>
+	);
+}
+
+export default Sec1;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { createRoot } from "react-dom/client";
 import Header from "./Mods/Header";
+import Sec1 from "./Mods/Sec1";
 import "./Style/main.css";
 
 createRoot(document.getElementById("root")).render(
 	<div>
 		<Header />
+		<Sec1 />
 	</div>
 );
